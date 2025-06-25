120bfa475895167d762cc3107fe33f31a119d0f8
Borey <85061131+boreymarf@users.noreply.github.com>
2025-06-25 14:22:00 +0300

    Macro's recording state is now visible on the lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,6 +1,6 @@
 return {
   'nvim-lualine/lualine.nvim',
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  dependencies = { 'nvim-tree/nvim-web-devicons', 'falke/noice.nvim' },
   opts = {
     options = {
       icons_enabled = true,
@@ -38,7 +38,26 @@ return {
       lualine_a = { 'mode' },
       lualine_b = { 'branch', 'diff', 'diagnostics' },
       lualine_c = { 'filename' },
-      lualine_x = { 'encoding', 'fileformat', 'filetype' },
+      -- lualine_x = { 'encoding', 'fileformat', 'filetype' },
+      -- lualine_x = {
+      --   require('noice').api.status.mode.get,
+      --   cond = require('noice').api.status.mode.has,
+      --   color = { fg = '#ff9e64' },
+      -- },
+      lualine_x = {
+        {
+          function()
+            if package.loaded['noice'] then
+              return require('noice').api.status.mode.get()
+            end
+            return ''
+          end,
+          cond = function()
+            return package.loaded['noice'] and require('noice').api.status.mode.has()
+          end,
+          color = { fg = '#ff9e64' },
+        },
+      },
       lualine_y = { 'progress' },
       lualine_z = { 'location' },
     },
