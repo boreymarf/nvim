4599a7297cd10e722e2f0409ea227f7df97f9f3e
Borey <85061131+boreymarf@users.noreply.github.com>
2025-07-13 21:46:39 +0300

    Removed ruff

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -254,7 +254,7 @@ return {
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
         'black',
-        'ruff',
+        -- 'ruff',
         'markdownlint',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
