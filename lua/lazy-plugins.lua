917f238c31cb7b8babeda3e0eb60e8aa281f9087
Borey <85061131+boreymarf@users.noreply.github.com>
2025-12-19 12:34:05 +0300

    Added flash-nvim

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -38,6 +38,7 @@ require('lazy').setup({
   require 'plugins.venv-selector',
   -- require 'plugins.minty',
   require 'plugins.nvim-colorizer',
+  require 'plugins.flash-nvim',
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
